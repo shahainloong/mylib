245b6d8063134d1476873b50df7d531663d6292c
shahainloong <shahailong@aliyun.com>
2019-10-21 22:00:33 +0800

    添加作者

--- src/main/java/com/ac/mylib/java/multithread/MultiThread.java
+++ src/main/java/com/ac/mylib/java/multithread/MultiThread.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
 
+/**
+ * @author Echo
+ */
 @Slf4j
 public class MultiThread {
     public static void main(String[] args) {
