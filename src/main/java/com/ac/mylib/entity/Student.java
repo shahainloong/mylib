9f0833c1934f202e46fabb71561aa1692cced497
shahainloong <shahailong@aliyun.com>
2019-11-18 23:04:03 +0800

    追加lombok知识

---
+++ src/main/java/com/ac/mylib/entity/Student.java
@@ -0,0 +1,22 @@
+package com.ac.mylib.entity;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+//@Accessors(chain = true)
+//@Setter
+//@Getter
+//@RequiredArgsConstructor(staticName = "of")
+@ToString
+@Builder
+public class Student {
+    @NonNull
+    private String name;
+    private String age;
+
+    public static void main(String[] args) {
+//        Student student = Student.of("Echo").setAge("28");
+        Student student = Student.builder().name("miaoyi").age("26").build();
+        System.out.println(student.toString());
+    }
+}
