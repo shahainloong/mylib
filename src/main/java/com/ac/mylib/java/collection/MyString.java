1e4bfa92e00c857987ed5cec8c91cad6b172f0ac
shahainloong <shahailong@aliyun.com>
2020-01-01 19:07:51 +0800

    增加logback

--- src/main/java/com/ac/mylib/java/collection/MyString.java
+++ src/main/java/com/ac/mylib/java/collection/MyString.java
@@ -1,10 +1,16 @@
 package com.ac.mylib.java.collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyString {
 	private Integer id;
 	private String name;
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyString.class);
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
+		sb.append(" hello ").append("world ").toString().trim();
+		LOGGER.info("hello my first log : {}", sb);
 		System.out.println(sb.append(" hello ").append("world ").toString().trim());
 	}
 
