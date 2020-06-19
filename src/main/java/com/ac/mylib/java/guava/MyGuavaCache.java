946d37aa19e84eb41fbca7b9a08027d4bada4def
Hailong Sha <hailong.sha@sap.com>
2020-06-19 09:55:28 +0800

    daily commit

--- src/main/java/com/ac/mylib/java/guava/MyGuavaCache.java
+++ src/main/java/com/ac/mylib/java/guava/MyGuavaCache.java
@@ -4,9 +4,7 @@ import ch.qos.logback.core.util.TimeUtil;
 import com.ac.mylib.controller.Employee;
 import com.ac.mylib.dao.UserDao;
 import com.ac.mylib.entity.User;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.common.cache.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
@@ -21,23 +19,32 @@ public class MyGuavaCache {
     private static UserDao userDao;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        LoadingCache<String, Employee> cache = CacheBuilder.newBuilder().maximumSize(5).expireAfterAccess(10, TimeUnit.MILLISECONDS).build(new CacheLoader<String, Employee>() {
+        // 创建一个cacheLoader，必须
+        CacheLoader<String, Employee> loader = new CacheLoader<String, Employee>() {
             @Override
             public Employee load(String s) throws Exception {
                 System.out.println("Hello Ada");
-                return new Employee("Ada", 1000, LocalDate.now());
+                return new Employee(s, 1000, LocalDate.now());
             }
-        });
+        };
+        // 可选的，失效时退出策略
+        RemovalListener<String, Employee> removalListener = new RemovalListener<String, Employee>() {
+            @Override
+            public void onRemoval(RemovalNotification<String, Employee> removal) {
+                System.out.println("Shuting down------");
+            }
+        };
+        LoadingCache<String, Employee> cache = CacheBuilder.newBuilder().maximumSize(5).expireAfterAccess(10, TimeUnit.MILLISECONDS).removalListener(removalListener).build(loader);
         Employee Ada01 = cache.get("Ada");
-        System.out.println("Employee is:" + Ada01);
+        System.out.println("Employee01 is:" + Ada01);
         TimeUnit.MILLISECONDS.sleep(11);
         Employee ada02 = cache.get("Ada");
-        System.out.println("Employee is:" + ada02);
+        System.out.println("Employee02 is:" + cache.asMap());
 
         Employee ada3 = cache.get("Bob");
-        System.out.println("Employee is:" + ada3);
+        System.out.println("Employee03 is:" + cache.asMap());
         Employee ada04 = cache.get("Ada");
-        System.out.println("Employee is:" + ada04);
+        System.out.println("Employee04 is:" + cache.stats());
         Employee ada05 = cache.get("Ada");
         System.out.println("Employee is:" + ada05);
         Employee ada06 = cache.get("Ada");
@@ -49,9 +56,7 @@ public class MyGuavaCache {
         Employee ada09 = cache.get("Ada");
         System.out.println("Employee is:" + ada09);
         Employee ada10 = cache.get("Ada");
-        System.out.println("Employee is:" + ada10);
-
-
+        System.out.println("Employee is:" + cache.asMap());
 
 
     }
---
+++ src/main/java/com/ac/mylib/util/EhcacheUtil.java
@@ -0,0 +1,97 @@
+package com.ac.mylib.util;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.expiry.Duration;
+import org.ehcache.expiry.Expirations;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * ClassName: EhcacheConfig
+ * Description: Ehcache配置
+ * Author: johnnybro@qq.com
+ * Date: 2018/05/18 14:30:36
+ */
+@Configuration
+@EnableCaching
+public class EhcacheUtil {
+    private static CacheManager cacheManager;
+
+    /**
+     * 初始化Ehcache缓存对象
+     */
+    public EhcacheUtil() {
+        System.out.println("[Ehcache配置初始化<开始>]");
+
+        // 配置默认缓存属性
+        CacheConfiguration<String, String> cacheConfiguration = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                // 缓存数据K和V的数值类型
+                // 在ehcache3.3中必须指定缓存键值类型,如果使用中类型与配置的不同,会报类转换异常
+                String.class, String.class,
+                ResourcePoolsBuilder
+                        .newResourcePoolsBuilder()
+                        //设置缓存堆容纳元素个数(JVM内存空间)超出个数后会存到offheap中
+                        .heap(1000L, EntryUnit.ENTRIES)
+                        //设置堆外储存大小(内存存储) 超出offheap的大小会淘汰规则被淘汰
+                        .offheap(100L, MemoryUnit.MB)
+                        // 配置磁盘持久化储存(硬盘存储)用来持久化到磁盘,这里设置为false不启用
+                        .disk(500L, MemoryUnit.MB, false)
+        ).withExpiry(Expirations.timeToLiveExpiration(
+                //设置缓存过期时间
+                Duration.of(30L, TimeUnit.SECONDS))
+        ).withExpiry(Expirations.timeToIdleExpiration(
+                //设置被访问后过期时间(同时设置和TTL和TTI之后会被覆盖,这里TTI生效,之前版本xml配置后是两个配置了都会生效)
+                Duration.of(60L, TimeUnit.SECONDS))
+                // 缓存淘汰策略 默认策略是LRU（最近最少使用）。你可以设置为FIFO（先进先出）或是LFU（较少使用）。
+                // 这块还没看
+        /*.withEvictionAdvisor(
+                new OddKeysEvictionAdvisor<Long, String>())*/
+        ).build();
+        // CacheManager管理缓存
+        cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                // 硬盘持久化地址
+                .with(CacheManagerBuilder.persistence("D:/ehcacheData"))
+                // 设置一个默认缓存配置
+                .withCache("defaultCache", cacheConfiguration)
+                //创建之后立即初始化
+                .build(true);
+
+        System.out.println("[Ehcache配置初始化<完成>]");
+    }
+
+    public static void main(String[] args) {
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("preConfigured",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                                ResourcePoolsBuilder.heap(100))
+                                .build())
+                .build(true);
+
+        Cache<Long, String> preConfigured
+                = cacheManager.getCache("preConfigured", Long.class, String.class);
+
+        Cache<Long, String> myCache = cacheManager.createCache("myCache",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class,
+                        ResourcePoolsBuilder.heap(100)).build());
+
+        myCache.put(1L, "da one!");
+        preConfigured.put(1L, "do one!");
+
+        String value = myCache.get(1L);
+        System.out.println(preConfigured.get(1L));
+
+        cacheManager.close();
+    }
+
+}
+
