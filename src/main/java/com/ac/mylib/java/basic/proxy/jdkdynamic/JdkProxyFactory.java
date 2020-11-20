667115eaa3c8bbaaf5677fa4d9538384a0d2c6b3
hailong.sha <hailong.sha@sap.com>
2020-11-20 10:45:01 +0800

    Update JdkProxyFactory.java

--- src/main/java/com/ac/mylib/java/basic/proxy/jdkdynamic/JdkProxyFactory.java
+++ src/main/java/com/ac/mylib/java/basic/proxy/jdkdynamic/JdkProxyFactory.java
@@ -10,21 +10,22 @@ import com.ac.mylib.java.basic.proxy.MySmsServiceImpl;
 /**
  * 这个是获取代理对象的工厂类，通过这个类的getProxy方法来获取对应的代理类
  * <p>JDK 动态代理类使用步骤</p>
- * <ol><li>
- * 定义一个接口及其实现类；</li><li>
- * 自定义{@link InvocationHandler InvocationHandler}并重写{@link InvocationHandler#invoke invoke}方法，在{@link InvocationHandler#invoke invoke}方法中我们会调用原生方法（被代理类的方法）并自定义一些处理逻辑；</li><li>
- * 通过{@link Proxy#newProxyInstance newProxyInstance}方法创建代理对象；
- * </li></ol>
+ * <ol>
+ * <li> 定义一个接口及其实现类；</li>
+ * <li> 自定义{@link InvocationHandler InvocationHandler}并重写{@link InvocationHandler#invoke invoke}方法，
+ *      在{@link InvocationHandler#invoke invoke}方法中我们会调用原生方法（被代理类的方法）并自定义一些处理逻辑；</li>
+ * <li> 通过{@link Proxy#newProxyInstance newProxyInstance}方法创建代理对象；</li>
+ * </ol>
  * JDK 动态代理有一个最致命的问题是其只能代理实现了接口的类!
  *
  * @see java.lang.reflect.Proxy#newProxyInstance
  */
 public class JdkProxyFactory {
   /**
-   * 主要通过`Proxy.newProxyInstance（）`方法获取某个类的代理对象
+   * 主要通过{@link java.lang.reflect.Proxy#newProxyInstance}方法获取某个类的代理对象
    *
-   * @param target
-   * @return
+   * @param target 需要代理的类
+   * @return 泛化的代理类对象Object，针对不同的类要做强制类型转换
    */
   public static Object getProxy(Object target) {
     return Proxy.newProxyInstance(target.getClass().getClassLoader(), // 定义代理类的类加载器
